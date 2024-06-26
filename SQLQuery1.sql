66dfda8550b89723d61171c40e0daccf85e52489
khaledkajo1 <90919697+khaledkajo1@users.noreply.github.com>
2024-06-26 14:37:50 +0300

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -69,13 +69,6 @@ CREATE TABLE dbo.renting
 
 
 
-/*BULK INSERT dbo.movies
-FROM 'https://assets.datacamp.com/production/repositories/4068/datasets/3eebf2a145b76fee37357bcd55ac54577c03c805/movies_181127_2.csv'
-WITH
-(
-    FIELDTERMINATOR = ',',
-    ROWTERMINATOR = '\n',
-    FIRSTROW = 2
-);*/
+
 
 
